5711424df13b221c5664b8106830df864c84e120
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-12-02 20:07:54 +0100

    add health system

---
+++ Assets/Core/Scripts/Health.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public int healtPoints = 2;
+
+	void OnCollisionEnter(Collision collision) {
+		GiveDamage bullet = collision.gameObject.GetComponent<GiveDamage>();
+
+		Debug.Log("collision");
+
+		if (bullet) {
+			healtPoints -= bullet.GetDamage();
+
+			Debug.Log("healtPoints: " + healtPoints);
+
+			if (healtPoints <= 0) {
+				Debug.Log("Player is dead");
+			}
+		}
+	}
+
+}
