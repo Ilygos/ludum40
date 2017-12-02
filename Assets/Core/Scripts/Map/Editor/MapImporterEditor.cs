02997f8d2e22c58f1d07ae7897f97b693173437e
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-12-02 15:35:55 +0100

    add map importer

---
+++ Assets/Core/Scripts/Map/Editor/MapImporterEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapImporter))]
+public class MapImporterEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		MapImporter mapImporter = (MapImporter)target;
+
+		base.OnInspectorGUI();
+
+		if (GUILayout.Button("Import")) {
+			mapImporter.Import();
+		}
+	}
+
+}
---
+++ Assets/Core/Scripts/Map/MapImporter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MapImporter : MonoBehaviour {
+
+	public GameObject mapToImport;
+
+	public void Import() {
+		Transform bouncingColliders = mapToImport.transform.Find("BouncingColliders");
+
+		// Replace BoxCollider2D to Collider2D
+		foreach (Transform colliderTransfrom in bouncingColliders) {
+			var collider2d = colliderTransfrom.GetComponent<BoxCollider2D>();
+
+			if (collider2d) {
+				Vector2 offset = collider2d.offset;
+				Vector2 size = collider2d.size;
+
+				DestroyImmediate(collider2d);
+
+				BoxCollider collider3d = colliderTransfrom.gameObject.AddComponent<BoxCollider>();
+
+				collider3d.center = new Vector3(offset.x, offset.y, -0.5f);
+				collider3d.size = new Vector3(size.x, size.y, 1);
+			}
+		}
+
+		// Rotate map to be in x, z plan
+		mapToImport.transform.rotation = Quaternion.Euler(90f, 0, 0);
+	}
+
+}
