5a2f393819f9c1ce04ac337820c2d23630b3cc6f
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-12-02 18:18:00 +0100

    add a dumb enemy

---
+++ Assets/Core/Scripts/Enemy/EnemyShoot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShoot : MonoBehaviour {
+
+	public float shootDelay = 1;
+	public GameObject bulletPrefab;
+	public Transform anchorGun;
+
+	Transform bulletsHolder;
+	Transform player;
+
+	void Start () {
+		player = GameObject.Find("/Environment/Actors/Player").transform;
+		bulletsHolder = GameObject.Find("/Environment/BulletsHolder").transform;
+
+		StartCoroutine(ShootCoroutine()); 
+	}
+	
+	void Update () {
+		
+	}
+
+	IEnumerator ShootCoroutine() {
+		while(true) { 
+			yield return new WaitForSeconds(shootDelay);
+
+			Shoot();
+		}
+	}
+
+	void Shoot() {
+		Instantiate(bulletPrefab, anchorGun.position, anchorGun.rotation, bulletsHolder);
+	}
+}
