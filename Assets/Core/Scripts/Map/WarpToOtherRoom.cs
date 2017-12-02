343c14a4b709d2bc5213afbb7a329e05a45fbe17
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-12-02 23:16:51 +0100

    add room transition

---
+++ Assets/Core/Scripts/Map/WarpToOtherRoom.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class WarpToOtherRoom : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("OnTriggerEnter");
+
+		if (other.gameObject.GetComponent<PlayerInput>()) {
+			Debug.Log("player");
+		}
+	}
+
+}
