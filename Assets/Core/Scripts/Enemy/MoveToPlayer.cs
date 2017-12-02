fa1c5144603a7a885c55aeab3101a003af6120d9
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-12-02 19:23:00 +0100

    add a following enemy

---
+++ Assets/Core/Scripts/Enemy/MoveToPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class MoveToPlayer : MonoBehaviour {
+
+	public float refreshPositionDelay = 1f;
+
+	Vector3 startPosition;
+	NavMeshAgent navMeshAgent;
+	Transform player;
+
+	void Start() {
+		navMeshAgent = GetComponent<NavMeshAgent>();
+		player = GameObject.Find("/Environment/Actors/Player").transform;
+
+		StartCoroutine(MoveTowardPlayer());
+	}
+
+	IEnumerator MoveTowardPlayer() {
+		while(true) {
+			yield return new WaitForSeconds(refreshPositionDelay);
+
+			navMeshAgent.destination = player.position;
+		}
+	}
+
+}
