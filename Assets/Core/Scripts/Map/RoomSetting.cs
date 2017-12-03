5cc8dc13ddef16457eed660ec9dcad50683e31ba
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-12-03 13:09:21 +0100

    add a basic room setting

---
+++ Assets/Core/Scripts/Map/RoomSetting.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+
+public class RoomSetting : MonoBehaviour {
+
+	public EnemyWave[] waves;
+
+	Transform enemiesHolder;
+	int currentWave = 0;
+
+	void Start () {
+		enemiesHolder = GameObject.Find("/Environment/Actors/Enemies").transform;
+		StartNewWave(currentWave);
+	}
+	
+	void Update () {
+	}
+
+	void StartNewWave(int waveNumber) {
+		GameObject[] enemyWave = waves[waveNumber].enemies;
+		int i = 0;
+
+		foreach (GameObject enemyPrefab in enemyWave) {
+			Vector3 position = new Vector3(-4f + (float)i, 0, -3);
+
+			Instantiate(enemyPrefab, position, enemyPrefab.transform.rotation, enemiesHolder);
+			i++;
+		}
+	}
+
+	[Serializable]
+	public class EnemyWave {
+		public GameObject[] enemies;
+	}
+
+}
