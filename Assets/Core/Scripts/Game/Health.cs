91436331ba1f54384400072be05c499c67af2d93
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-12-03 15:59:50 +0100

    add a room manager

--- Assets/Core/Scripts/Game/Health.cs
+++ Assets/Core/Scripts/Game/Health.cs
@@ -3,35 +3,36 @@ using System;
 
 public class Health : MonoBehaviour {
 
-    public int HEALTH_POINT = 2;
-    public AudioClip deathSound;
-    public event Action OnDead;
+	public int HEALTH_POINT = 2;
+	public AudioClip deathSound;
 
-    AudioSource _audio;
+	public event Action OnDead;
 
-    private void Start()
-    {
-        _audio = GetComponent<AudioSource>();
-    }
+	AudioSource _audio;
 
-    void OnCollisionEnter(Collision collision) {
+	private void Start() {
+		_audio = GetComponent<AudioSource>();
+	}
+
+	void OnCollisionEnter(Collision collision) {
 		GiveDamage bullet = collision.gameObject.GetComponent<GiveDamage>();
 
 		if (bullet) {
-            Debug.Log(gameObject.name + " " + HEALTH_POINT );
-            HEALTH_POINT -= bullet.GetDamage();
+			Debug.Log(gameObject.name + " " + HEALTH_POINT);
+			HEALTH_POINT -= bullet.GetDamage();
 
 			if (HEALTH_POINT <= 0) {
-        _audio.clip = deathSound;
-        _audio.Play();
+				_audio.clip = deathSound;
+				_audio.Play();
+
+				if (OnDead != null) {
+					OnDead.Invoke();
+				}
+
 				if (gameObject.tag == "Player") {
 					UIManager.Instance.loose();
 				} else {
 					DestroyObject(gameObject);
-				}
-
-				if (OnDead != null) {
-					OnDead.Invoke();
 				}
 			}
 		}
---
+++ Assets/RoomManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class RoomManager : MonoBehaviour {
+
+	public GameObject[] rooms;
+
+	int currentRoom = 0;
+
+	void Start () {
+		
+	}
+	
+	public void GoToNext() {
+		currentRoom++;
+
+		if (currentRoom >= rooms.Length) {
+			Debug.Log("You Win");
+		} else {
+			Debug.Log("Go to next room");
+
+			GameObject nextRoom = rooms[currentRoom];
+//			Transform playerSpawn = 
+		}
+	}
+}
