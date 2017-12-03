5b99282c4e41638a25db6730a998b0d9293ea6cc
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-12-03 02:23:29 +0100

    add mouse aiming

---
+++ Assets/Core/Scripts/Mouse/MouseCursorManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MouseCursorManager : MonoBehaviour {
+
+	public Texture2D normalCursor;
+	public Vector2 normalHotspot;
+
+	void Start () {
+		Cursor.SetCursor(normalCursor, normalHotspot, CursorMode.ForceSoftware);
+	}
+
+}
